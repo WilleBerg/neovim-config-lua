07ab0ed3aca73f213b7c8adf19d22b0b2fb0cf94
jammy <william.berg2@gmail.com>
2023-10-19 13:03:00 +0200

    from laptop

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,8 +1,20 @@
 local color = {}
 color.lualine_colorscheme = "gruvbox_dark"
 -- color.lualine_colorscheme = "catppuccin"
+--
+color.colorscheme = "gruvbox"
+-- color.colorscheme = "catppuccin"
 
-vim.cmd.colorscheme "gruvbox"
--- vim.cmd.colorscheme "catppuccin"
+vim.cmd("colorscheme " .. color.colorscheme)
+
+function ColorMyPencils(colour)
+     colour = colour or color.colorscheme
+     vim.cmd.colorscheme(colour)
+
+     vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
+     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
+ end
+
+ ColorMyPencils()
 
 return color
