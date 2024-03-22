19d29a91374485cc43bd1e2c71a688b44761726d
WilleBerg <william.berg2@gmail.com>
2024-03-22 11:47:48 +0100

    Added todo comments plugin

--- lua/sylt/lazy.lua
+++ lua/sylt/lazy.lua
@@ -94,6 +94,15 @@ plugins = {
             'RubixDev/ebnf',
             rtp = 'crates/tree-sitter-ebnf',
     },
+    {
+      "folke/todo-comments.nvim",
+      dependencies = { "nvim-lua/plenary.nvim" },
+      opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+}
 }
 
 require("lazy").setup(plugins)
