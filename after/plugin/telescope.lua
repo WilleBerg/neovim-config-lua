7a45016fe36d85c00b741f6f0e86c478aae59a05
WilleBerg <william.berg2@gmail.com>
2024-08-12 17:02:33 +0200

    Added keybind lookup <leader>kb

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,8 +1,8 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>pz', function() 
+vim.keymap.set('n', '<leader>pz', function()
     builtin.grep_string( {search = vim.fn.input("Grep > ")})
-end)
+end, {desc="Grep a string"})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>pa', function()
 	builtin.find_files({
@@ -20,4 +20,12 @@ vim.keymap.set('n', '<leader>sz', function()
 		find_command = { "rg", "--files", "--hidden", "--ignore", "!**/.git/*" },
 		cwd = "~/.config/nvim",
 	})
-end)
+end, {desc="Live greps .config/nvim"})
+vim.keymap.set('n', '<leader>lg', function()
+	builtin.live_grep({
+		find_command = { "rg", "--files", "--hidden", "--ignore", "!**/.git/*" },
+	})
+end, {desc="Live greps current working directory"})
+vim.keymap.set('n', '<leader>kb', function()
+	builtin.keymaps()
+end, {desc="Lists all keybinds"})
--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -8,6 +8,7 @@ require'nvim-treesitter.configs'.setup {
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
+  
 
   highlight = {
     enable = true,
