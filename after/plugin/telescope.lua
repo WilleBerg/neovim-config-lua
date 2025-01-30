f5b40db7a61294cbfaa121e7928d641cf1d6cf90
willeberg <william.berg2@gmail.com>
2025-01-30 16:42:25 +0100

    Added view diagnostics

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,18 +3,12 @@ vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pz', function()
     builtin.grep_string( {search = vim.fn.input("Grep > ")})
 end, {desc="Grep a string"})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {desc="Show all git files"})
 vim.keymap.set('n', '<leader>pa', function()
 	builtin.find_files({
 		find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*", "--glob", "!**/node_modules/*" },
 	})
-end)
-vim.keymap.set('n', '<leader>sf', function()
-	builtin.find_files({
-		find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*", "--glob", "!**/node_modules/*"},
-		cwd = "~/",
-	})
-end)
+end, {desc="Show all files"})
 vim.keymap.set('n', '<leader>sz', function()
 	builtin.live_grep({
 		find_command = { "rg", "--files", "--hidden", "--ignore", "!**/.git/*" },
@@ -29,3 +23,6 @@ end, {desc="Live greps current working directory"})
 vim.keymap.set('n', '<leader>kb', function()
 	builtin.keymaps()
 end, {desc="Lists all keybinds"})
+vim.keymap.set('n', '<leader>vd', function()
+    builtin.diagnostics({bufnr=0, severity_bound = 0})
+end, {desc="View diagnostics"})
