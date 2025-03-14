3a4baee2f7068709e5ef7764307624a017c4192f
William Berg <william.berg2@gmail.com>
2025-03-14 11:38:18 +0100

    Added rustaceanvim

--- init.lua
+++ init.lua
@@ -5,6 +5,23 @@ local config_dir = vim.fn.stdpath('config')
 package.path = package.path .. ';' .. config_dir .. '/?.lua'
 
 
+vim.g.rustaceanvim = {
+  -- LSP configuration
+  server = {
+    on_attach = function(client, bufnr)
+      -- you can also put keymaps in here
+    end,
+    default_settings = {
+      -- rust-analyzer language server configuration
+      ['rust-analyzer'] = {
+          cargo = {
+            features = {
+                "all_queues"
+            }
+          }
+      },
+    },
+  },
+}
 require("sylt")
 require('leap').add_default_mappings()
-
