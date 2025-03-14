fe0599947c1985ccdd83c1918f63924a6ed6ce7a
William Berg <william.berg2@gmail.com>
2025-03-14 11:37:25 +0100

    Added rustaceanvim, harpoon 2, troubles

--- lua/sylt/lazy.lua
+++ lua/sylt/lazy.lua
@@ -25,7 +25,9 @@ plugins = {
 		'nvim-treesitter/nvim-treesitter',
 	},
 	{
-		'ThePrimeagen/harpoon',
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim" }
 	},
 	{
 		'mbbill/undotree'
@@ -102,7 +104,52 @@ plugins = {
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
       }
-}
+    },
+    {
+      "folke/trouble.nvim",
+      opts = {}, -- for default options, refer to the configuration section for custom setup.
+      cmd = "Trouble",
+      keys = {
+        {
+          "<leader>xx",
+          "<cmd>Trouble diagnostics toggle<cr>",
+          desc = "Diagnostics (Trouble)",
+        },
+        {
+          "<leader>xX",
+          "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+          desc = "Buffer Diagnostics (Trouble)",
+        },
+        {
+          "<leader>cs",
+          "<cmd>Trouble symbols toggle focus=false<cr>",
+          desc = "Symbols (Trouble)",
+        },
+        {
+          "<leader>cl",
+          "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+          desc = "LSP Definitions / references / ... (Trouble)",
+        },
+        {
+          "<leader>xL",
+          "<cmd>Trouble loclist toggle<cr>",
+          desc = "Location List (Trouble)",
+        },
+        {
+          "<leader>xQ",
+          "<cmd>Trouble qflist toggle<cr>",
+          desc = "Quickfix List (Trouble)",
+        },
+      },
+    },
+    {
+      'mrcjkb/rustaceanvim',
+      version = '^5', -- Recommended
+      lazy = false, -- This plugin is already lazy
+    },
+    -- {
+    --     'mfussenegger/nvim-dap'
+    -- }
 }
 
 require("lazy").setup(plugins)
